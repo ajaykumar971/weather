762243fdc717ed453bd880119b4546b0eea2ad06
ajaykumar971 <101116375+ajaykumar971@users.noreply.github.com>
2022-12-09 21:30:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,15 +44,15 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Hyderabad', 
           console.log(response)
 
           // cloud_pct.innerHTML = response.cloud_pct
-          hyd1.innerHTML = response.temp
-          hyd2.innerHTML = response.feels_like
-          hyd3.innerHTML = response.humidity
+          hyd1.innerHTML = response.feels_like
+          hyd2.innerHTML = response.humidity
+          hyd3.innerHTML = response.max_temp
           hyd4.innerHTML = response.min_temp
-          hyd5.innerHTML = response.max_temp
-          hyd6.innerHTML = response.wind_speed
-          hyd7.innerHTML = response.wind_degrees
-          hyd8.innerHTML = response.sunrise
-          hyd9.innerHTML = response.sunset
+          hyd5.innerHTML = response.sunrise
+          hyd6.innerHTML = response.sunset
+          hyd7.innerHTML = response.temp
+          hyd8.innerHTML = response.wind_degrees
+          hyd9.innerHTML = response.wind_speed
      })
      .catch(err => console.error(err));
 
@@ -64,15 +64,15 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=lucknow', op
           console.log(response)
 
           // cloud_pct.innerHTML = response.cloud_pct
-          luck1.innerHTML = response.temp
-          luck2.innerHTML = response.feels_like
-          luck3.innerHTML = response.humidity
+          luck1.innerHTML = response.feels_like
+          luck2.innerHTML = response.humidity
+          luck3.innerHTML = response.max_temp
           luck4.innerHTML = response.min_temp
-          luck5.innerHTML = response.max_temp
-          luck6.innerHTML = response.wind_speed
-          luck7.innerHTML = response.wind_degrees
-          luck8.innerHTML = response.sunrise
-          luck9.innerHTML = response.sunset
+          luck5.innerHTML = response.sunrise
+          luck6.innerHTML = response.sunset
+          luck7.innerHTML = response.temp
+          luck8.innerHTML = response.wind_degrees
+          luck9.innerHTML = response.wind_speed
      })
      .catch(err => console.error(err));
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=chennai', options)
@@ -82,15 +82,15 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=chennai', op
           console.log(response)
 
           // cloud_pct.innerHTML = response.cloud_pct
-          chen1.innerHTML = response.temp
-          chen2.innerHTML = response.feels_like
-          chen3.innerHTML = response.humidity
+          chen1.innerHTML = response.feels_like
+          chen2.innerHTML = response.humidity
+          chen3.innerHTML = response.max_temp
           chen4.innerHTML = response.min_temp
-          chen5.innerHTML = response.max_temp
-          chen6.innerHTML = response.wind_speed
-          chen7.innerHTML = response.wind_degrees
-          chen8.innerHTML = response.sunrise
-          chen9.innerHTML = response.sunset
+          chen5.innerHTML = response.sunrise
+          chen6.innerHTML = response.sunset
+          chen7.innerHTML = response.temp
+          chen8.innerHTML = response.wind_degrees
+          chen9.innerHTML = response.wind_speed
      })
      .catch(err => console.error(err));
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Bhubaneswar', options)
@@ -100,14 +100,14 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Bhubaneswar'
           console.log(response)
 
           // cloud_pct.innerHTML = response.cloud_pct
-          gn1.innerHTML = response.temp
-          gn2.innerHTML = response.feels_like
-          gn3.innerHTML = response.humidity
+          gn1.innerHTML = response.feels_like
+          gn2.innerHTML = response.humidity
+          gn3.innerHTML = response.max_temp
           gn4.innerHTML = response.min_temp
-          gn5.innerHTML = response.max_temp
-          gn6.innerHTML = response.wind_speed
-          gn7.innerHTML = response.wind_degrees
-          gn8.innerHTML = response.sunrise
-          gn9.innerHTML = response.sunset
+          gn5.innerHTML = response.sunrise
+          gn6.innerHTML = response.sunset
+          gn7.innerHTML = response.temp
+          gn8.innerHTML = response.wind_degrees
+          gn9.innerHTML = response.wind_speed
      })
      .catch(err => console.error(err));
